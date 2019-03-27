dbea8081ab4814beeb13151f0eda2cb8e29a290f
reese0329 <106078014@qq.com>
2019-03-27 18:10:31 +0800

    0

--- baidu_login.py
+++ baidu_login.py
@@ -1,48 +1,54 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.keys import Keys
+import unittest
 #添加断言
 
-
-driver = webdriver.Chrome()
-driver.get("https://www.baidu.com")
-driver.maximize_window()
-
-driver.find_element_by_css_selector("#u1 > a.lb").click()
-sleep(2)  #添加等待时间，等待弹窗展示
+class LoginBaidu(unittest.TestCase):
 
 
-# # a = driver.find_element_by_class_name("pass-form-logo")  #登录弹窗展示，不能获取文本信息
-# # print(a.text)
-# sleep(1)
+    def login(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.baidu.com")
+        self.driver.maximize_window()
 
-driver.find_element_by_id("TANGRAM__PSP_10__footerULoginBtn").click()
+        self.driver.find_element_by_css_selector("#u1 > a.lb").click()
+        sleep(2)  #添加等待时间，等待弹窗展示
 
-#用户名密码登录
-driver.find_element_by_id("TANGRAM__PSP_10__userName").clear()
-driver.find_element_by_id("TANGRAM__PSP_10__userName").send_keys("13426257806")
-driver.find_element_by_id("TANGRAM__PSP_10__password").clear()
-driver.find_element_by_id("TANGRAM__PSP_10__password").send_keys("13426257806")
-driver.find_element_by_id("TANGRAM__PSP_10__submit").click()
-sleep(3)
 
-driver.find_element_by_id("TANGRAM__39__button_send_mobile").click()
-keys = input("输入验证码：")
-# driver.find_element_by_id("TANGRAM__39__input_label_vcode").clear()  #不能获取验证码输入框
-# driver.find_element_by_id("TANGRAM__39__input_label_vcode").send_keys(keys)
-# sleep(3)
-driver.find_element_by_id("TANGRAM__39__button_submit").click()
-print("login!")
-sleep(4)
+        a = self.driver.find_element_by_class_name("pass-form-logo").text  #登录弹窗展示，不能获取文本信息
+        self.assertEqual("扫码登录",a,msg="当前弹窗应为{}，实际为{}".format("扫码登录",a))
 
-# #短信验证码登录
-# driver.find_element_by_id("TANGRAM__PSP_10__smsSwitchWrapper").click()
-# driver.find_element_by_id("TANGRAM__PSP_10__smsPhone").clear()
-# driver.find_element_by_id("TANGRAM__PSP_10__smsPhone").send_keys("13426257806")
-# code = driver.find_element_by_id("TANGRAM__PSP_10__smsTimer").click()
-# keys = input("输入验证码：")
-# driver.find_element_by_id("TANGRAM__PSP_10__smsVerifyCode").clear()
-# driver.find_element_by_id("TANGRAM__PSP_10__smsVerifyCode").send_keys(keys)
-# sleep(3)
-# driver.find_element_by_id("TANGRAM__PSP_10__smsSubmit").click()
-# print("login!")
+
+        sleep(1)
+
+        driver.find_element_by_id("TANGRAM__PSP_10__footerULoginBtn").click()
+
+        #用户名密码登录
+        driver.find_element_by_id("TANGRAM__PSP_10__userName").clear()
+        driver.find_element_by_id("TANGRAM__PSP_10__userName").send_keys("13426257806")
+        driver.find_element_by_id("TANGRAM__PSP_10__password").clear()
+        driver.find_element_by_id("TANGRAM__PSP_10__password").send_keys("13426257806")
+        driver.find_element_by_id("TANGRAM__PSP_10__submit").click()
+        sleep(3)
+
+        driver.find_element_by_id("TANGRAM__39__button_send_mobile").click()
+        keys = input("输入验证码：")
+        # driver.find_element_by_id("TANGRAM__39__input_label_vcode").clear()  #不能获取验证码输入框
+        # driver.find_element_by_id("TANGRAM__39__input_label_vcode").send_keys(keys)
+        # sleep(3)
+        driver.find_element_by_id("TANGRAM__39__button_submit").click()
+        print("login!")
+        sleep(4)
+
+        # #短信验证码登录
+        # driver.find_element_by_id("TANGRAM__PSP_10__smsSwitchWrapper").click()
+        # driver.find_element_by_id("TANGRAM__PSP_10__smsPhone").clear()
+        # driver.find_element_by_id("TANGRAM__PSP_10__smsPhone").send_keys("13426257806")
+        # code = driver.find_element_by_id("TANGRAM__PSP_10__smsTimer").click()
+        # keys = input("输入验证码：")
+        # driver.find_element_by_id("TANGRAM__PSP_10__smsVerifyCode").clear()
+        # driver.find_element_by_id("TANGRAM__PSP_10__smsVerifyCode").send_keys(keys)
+        # sleep(3)
+        # driver.find_element_by_id("TANGRAM__PSP_10__smsSubmit").click()
+        # print("login!")
---
+++ test_loginDouban.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+import os, time, unittest, configparser
+
+
+class LoginDouban(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):  # 每轮执行case的开始
+        cls.driver = webdriver.Chrome()
+        cls.driver.get("https://www.douban.com")
+        # cls.driver.implicitly_wait (5)  # 隐式等待5s
+
+    @classmethod
+    def tearDownClass(cls):  # 每轮执行case的结束
+        print("this is the end!")
+        cls.driver.quit()
+
+    # 保护代码
+    def test_login(self):
+        time.sleep (4)
+        try:
+            self.assertEqual("豆瓣", self.driver.title, msg="当前网页的{}和{}一样".format("豆瓣", self.driver.title))
+            self.assertEqual("https://www.douban.com/", self.driver.current_url,
+                                  msg="当前网页的{}和{}一样".format ("https://www.douban.com", self.driver.current_url))
+            obj = self.driver.__sizeof__()
+            print(obj)
+            time.sleep(2)
+            if int(obj / 1024) > 60:
+                print(self.driver.current_url,"网页过大")
+                #     fp = open("../test/sizelog.txt", "w", encoding="utf-8")  # 自己封装,拼接路径，w-写入
+                #     fp.write(get_ini_date("Language", "size").format(self.driver.current_url))  #写入内容
+                # #     fp.close()
+            check_login = self.driver.find_element_by_link_text("lnk-fm")
+            if check_login.is_enabled():
+                self.assertEqual("豆瓣FM", check_login.text, msg="登录element{}".format(check_login.text))
+            else:
+                raise "{}不可点击".format(check_login.text)
+            check_login.click ()
+            time.sleep(5)
+        except Exception as err:
+            print(format(err))
+
+
+if __name__ == '__main__':
+    unittest.main()
